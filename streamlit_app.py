3582090845392825b9d55a0eef962f1eb9c1bba4
luigi0203 <poluis@hotmail.com>
2024-04-07 17:36:43 -0500

    Update streamlit_app.py
    Get the current credentials

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+# Get the current credentials
+session = get_active_session()
+
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
 st.header("Breakfast Menu")
@@ -17,7 +20,6 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
