46117bcae673e156bb0c084d266e2f1d12d9959d
luigi0203 <poluis@hotmail.com>
2024-04-07 20:13:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake_connection import get_active_session
+from snowflake.connection import get_active_session
 from snowflake.snowpark.functions import col
+
 # Get the current credentials
 session = get_active_session()
 
