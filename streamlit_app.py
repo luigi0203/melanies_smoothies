060be8e44e276b33896bdb5720362d6caffd1372
luigi0203 <poluis@hotmail.com>
2024-04-07 20:25:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-import streamlit as st
+#import streamlit as st
 #import snowflake.connector
 #from snowflake.connection import get_active_session
 #from snowflake.snowpark.functions import col
